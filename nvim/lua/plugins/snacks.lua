29fba1497efecb906148f1b00543e76436c53b55
edwardwq301 <edwardwq301@qq.com>
2025-11-02 17:57:29 +0800

    snack add dependencies: web-devicons

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -2,6 +2,9 @@ return {
   "folke/snacks.nvim",
   priority = 1000,
   lazy = false,
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+  },
   ---@type snacks.Config
   opts = {
     bigfile = { enabled = true },
