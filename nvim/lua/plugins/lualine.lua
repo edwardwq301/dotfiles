ee8269267e1813f9151f4c8605fe91c5d85e07f5
edwardwq301 <edwardwq301@qq.com>
2025-11-03 23:05:37 +0800

    set tabline

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -3,33 +3,55 @@ return {
   config = function()
     require('lualine').setup({
       options = {
-        theme = 'auto',  
-        icons_enabled = true,  
+        theme = 'auto',
+        icons_enabled = true,
         disabled_filetypes = {
           statusline = {},
           winbar = {},
         },
       },
+      -- bottom
       sections = {
-        lualine_a = {'mode'},
-        lualine_b = {'branch', 'diff', 'diagnostics'},
-        lualine_c = {'filename'},
+        lualine_a = { 'mode' },
+        lualine_b = { 'branch', 'diff', 'diagnostics' },
+        lualine_c = {},
         lualine_x = {
           'encoding',
           -- 'fileformat',   -- unix, windows, apple
           'filetype'
         },
-        lualine_y = {'progress'},
-        lualine_z = {'location'}
+        lualine_y = { 'progress' },
+        lualine_z = { 'location' }
       },
       inactive_sections = {
         lualine_a = {},
         lualine_b = {},
-        lualine_c = {'filename'},
-        lualine_x = {'location'},
+        lualine_c = { 'filename' },
+        lualine_x = { 'location' },
         lualine_y = {},
         lualine_z = {}
       },
+
+      tabline = {
+        lualine_a = {},
+        lualine_b = { 'branch' },
+        lualine_c = { 'filename' },
+        lualine_x = {},
+        lualine_y = {},
+        lualine_z = {
+          {
+            'tabs',
+            mode = 2,
+            tabs_color = {
+              active = 'lualine_a_normal',     -- 使用 lualine 主题的活动颜色
+              inactive = 'lualine_a_inactive', -- 使用 lualine 主题的非活动颜色
+            },
+          }
+        }
+      },
+
+      winbar = {},
+      inactive_winbar = {},
     })
   end
 }
